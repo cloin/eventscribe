dc0d711768c719d5700d680ed2d10bef59f7ef73
Colin McNaughton <c.a.mcnaughton@gmail.com>
2023-09-20 16:25:15 -0400

    initial

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,108 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [payloads, setPayloads] = useState([]);
+  const [selectedPayload, setSelectedPayload] = useState(null);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedPath, setSelectedPath] = useState('');
+  const [addEventKey, setAddEventKey] = useState(true);
+
+  // grab payloads from the server
+  useEffect(() => {
+    const fetchPayloads = () => {
+      fetch('http://localhost:3331/payloads')
+        .then((res) => res.json())
+        .then((data) => setPayloads(data))
+        .catch((err) => console.error(err));
+    };
+
+    fetchPayloads();  // fetch once initially
+    const intervalId = setInterval(fetchPayloads, 5000);  // fetch every 5 seconds
+
+    return () => clearInterval(intervalId);  // cleanup interval on component unmount
+  }, []);
+
+  // display JSON payload and its dot-notation path with search highlighting
+  const displayJSON = (obj, prevKey = '') => {
+    return Object.keys(obj).map((key, index) => {
+      const value = obj[key];
+      const newKey = prevKey ? `${prevKey}.${key}` : key;
+
+      const highlight = (text) => {
+        if (searchTerm && text.toLowerCase().includes(searchTerm.toLowerCase())) {
+          return <span className="highlight">{text}</span>;
+        }
+        return text;
+      };
+
+      if (typeof value === 'object') {
+        return (
+          <div key={index}>
+            <span>{highlight(key)}: {'{'}</span>
+            <div style={{ marginLeft: '20px' }}>{displayJSON(value, newKey)}</div>
+            <span>{'}'}</span>
+          </div>
+        );
+      }
+      return (
+        <div key={index} onClick={() => setSelectedPath(newKey)}>
+          {highlight(key)}: {highlight(JSON.stringify(value))}
+        </div>
+      );
+    });
+  };
+
+  const handleCopyClick = () => {
+    const finalPath = addEventKey ? `event.${selectedPath}` : selectedPath;
+    navigator.clipboard.writeText(finalPath);
+  };
+
+  const displayedPath = addEventKey && selectedPath ? `event.${selectedPath}` : selectedPath;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="left-pane">
+        <div className="search-box">
+          <input
+            type="text"
+            placeholder="Search payloads..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button onClick={() => setSearchTerm('')}>Clear</button>
+        </div>
+        {payloads
+          .filter((payloadWrapper) => JSON.stringify(payloadWrapper.payload).toLowerCase().includes(searchTerm.toLowerCase()))
+          .map((payloadWrapper, index) => (
+            <div
+              key={index}
+              className="payload-item"
+              onClick={() => setSelectedPayload(payloadWrapper.payload)}
+            >
+              {index + 1}. {payloadWrapper.timestamp}
+            </div>
+          ))}
+      </div>
+      <div className="right-pane">
+        <div className="clipboard-section">
+          <input 
+            type="text" 
+            readOnly 
+            value={displayedPath || 'Select JSON key/value...'} 
+          />
+          <label className="checkbox-label">
+            <input 
+              type="checkbox" 
+              checked={addEventKey} 
+              onChange={() => setAddEventKey(!addEventKey)} 
+            />
+            Prepend event
+          </label>
+          <button onClick={handleCopyClick}>📋 Copy</button>
+        </div>
+        {selectedPayload && displayJSON(selectedPayload)}
+      </div>
     </div>
   );
 }
--- server/app.js
+++ server/app.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
-const port = 3001;
+const port = 3331;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(cors()); 
+app.use(bodyParser.json());
+
+// store payloads in-memory
+const payloads = [];
+
+app.post('/webhook', (req, res) => {
+  const payload = req.body;
+  const timestamp = new Date().toLocaleString();
+  payloads.push({ payload, timestamp });
+  console.log(`Received payload at ${timestamp}: ${JSON.stringify(payload)}`);
+  res.status(200).send('OK');
 });
+
+app.get('/payloads', (req, res) => {
+  res.json(payloads);
+});
+
 
 app.listen(port, () => {
-  console.log();
+  console.log(`Server running at http://localhost:${port}/`);
 });
+
