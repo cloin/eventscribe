80f94329271fc1bec78ee668e091465e504e8a8c
Colin McNaughton <c.a.mcnaughton@gmail.com>
2023-09-26 15:14:10 -0400

    add rulebook generation

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,62 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
+const PayloadDisplay = ({ obj, onKeyClick }) => {
+  const displayJSON = (obj, prevKey = '') => 
+    Object.keys(obj).map((key, index) => {
+      const value = obj[key];
+      const newKey = prevKey ? `${prevKey}.${key}` : key;
+
+      if (typeof value === 'object') {
+        return (
+          <div key={index}>
+            <span>{key}: {'{'}</span>
+            <div style={{ marginLeft: '20px' }}>{displayJSON(value, newKey)}</div>
+            <span>{'}'}</span>
+          </div>
+        );
+      }
+      return (
+        <div 
+          key={index} 
+          onClick={() => onKeyClick(newKey, value)}
+        >
+          {key}: {JSON.stringify(value)}
+        </div>
+      );
+    });
+
+  return <>{displayJSON(obj)}</>;
+}
+
+const RuleCreator = ({ path, ruleName, condition, ruleValue, onGenerate, onChange }) => {
+  return (
+    <div className="rule-creator">
+      <input type="text" placeholder="Rule Name" value={ruleName} onChange={(e) => onChange('ruleName', e.target.value)} />
+      <input type="text" placeholder="Path" value={path} onChange={(e) => onChange('path', e.target.value)} />
+      <select value={condition} onChange={(e) => onChange('condition', e.target.value)}>
+        <option value="==">==</option>
+        <option value="!=">!=</option>
+      </select>
+      <input type="text" placeholder="Value" value={ruleValue} onChange={(e) => onChange('ruleValue', e.target.value)} />
+      <button onClick={onGenerate}>Generate Rule</button>
+    </div>
+  );
+}
+
 function App() {
   const [payloads, setPayloads] = useState([]);
   const [selectedPayload, setSelectedPayload] = useState(null);
   const [searchTerm, setSearchTerm] = useState('');
   const [selectedPath, setSelectedPath] = useState('');
   const [addEventKey, setAddEventKey] = useState(true);
+  const [ruleName, setRuleName] = useState("");
+  const [condition, setCondition] = useState("==");
+  const [ruleValue, setRuleValue] = useState("");
+  const [generatedRule, setGeneratedRule] = useState("");
+  const [isLeftPaneVisible, setIsLeftPaneVisible] = useState(true);
+  const [path, setPath] = useState('');
 
-  // grab payloads from the server
   useEffect(() => {
     const fetchPayloads = () => {
       fetch('http://localhost:3331/payloads')
@@ -17,79 +65,86 @@ function App() {
         .catch((err) => console.error(err));
     };
 
-    fetchPayloads();  // fetch once initially
-    const intervalId = setInterval(fetchPayloads, 5000);  // fetch every 5 seconds
+    fetchPayloads();
+    const intervalId = setInterval(fetchPayloads, 5000);
 
-    return () => clearInterval(intervalId);  // cleanup interval on component unmount
+    return () => clearInterval(intervalId);
   }, []);
 
-  // display JSON payload and its dot-notation path with search highlighting
-  const displayJSON = (obj, prevKey = '') => {
-    return Object.keys(obj).map((key, index) => {
-      const value = obj[key];
-      const newKey = prevKey ? `${prevKey}.${key}` : key;
+  const handleKeyClick = (newKey, value) => {
+    setSelectedPath(newKey);
+    setPath(newKey);
+    setRuleValue(JSON.stringify(value));
+  }
 
-      const highlight = (text) => {
-        if (searchTerm && text.toLowerCase().includes(searchTerm.toLowerCase())) {
-          return <span className="highlight">{text}</span>;
-        }
-        return text;
-      };
-
-      if (typeof value === 'object') {
-        return (
-          <div key={index}>
-            <span>{highlight(key)}: {'{'}</span>
-            <div style={{ marginLeft: '20px' }}>{displayJSON(value, newKey)}</div>
-            <span>{'}'}</span>
-          </div>
-        );
-      }
-      return (
-        <div key={index} onClick={() => setSelectedPath(newKey)}>
-          {highlight(key)}: {highlight(JSON.stringify(value))}
-        </div>
-      );
-    });
+  const handleChange = (field, value) => {
+    switch (field) {
+      case 'path':
+        setPath(value);
+        break;
+      case 'ruleName':
+        setRuleName(value);
+        break;
+      case 'condition':
+        setCondition(value);
+        break;
+      case 'ruleValue':
+        setRuleValue(value);
+        break;
+      default:
+        break;
+    }
   };
 
-  const handleCopyClick = () => {
-    const finalPath = addEventKey ? `event.${selectedPath}` : selectedPath;
-    navigator.clipboard.writeText(finalPath);
+  const generateRule = () => {
+    const ruleTemplate = `
+- name: ${ruleName}
+  condition: event.${path} ${condition} ${ruleValue}
+  action:
+    debug:
+    `;
+    setGeneratedRule(ruleTemplate);
   };
 
-  const displayedPath = addEventKey && selectedPath ? `event.${selectedPath}` : selectedPath;
+  const handleCopyRule = () => {
+    navigator.clipboard.writeText(generatedRule);
+  }
 
   return (
     <div className="App">
-      <div className="left-pane">
-        <div className="search-box">
-          <input
-            type="text"
-            placeholder="Search payloads..."
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          <button onClick={() => setSearchTerm('')}>Clear</button>
+      {isLeftPaneVisible && (
+        <div className="left-pane">
+          <div className="search-box">
+            <input
+              type="text"
+              placeholder="Search payloads..."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <button onClick={() => setSearchTerm('')}>Clear</button>
+          </div>
+          {payloads
+            .filter((payloadWrapper) => JSON.stringify(payloadWrapper.payload).toLowerCase().includes(searchTerm.toLowerCase()))
+            .map((payloadWrapper, index) => (
+              <div
+                key={index}
+                className="payload-item"
+                onClick={() => setSelectedPayload(payloadWrapper.payload)}
+              >
+                {index + 1}. {payloadWrapper.timestamp}
+              </div>
+            ))}
         </div>
-        {payloads
-          .filter((payloadWrapper) => JSON.stringify(payloadWrapper.payload).toLowerCase().includes(searchTerm.toLowerCase()))
-          .map((payloadWrapper, index) => (
-            <div
-              key={index}
-              className="payload-item"
-              onClick={() => setSelectedPayload(payloadWrapper.payload)}
-            >
-              {index + 1}. {payloadWrapper.timestamp}
-            </div>
-          ))}
+      )}
+      <div className="toggle-pane-button" onClick={() => setIsLeftPaneVisible(!isLeftPaneVisible)}>
+        {isLeftPaneVisible ? 'Hide' : 'Show'}
       </div>
-      <div className="right-pane">
-        <div className="clipboard-section">
+      <div className="middle-pane">
+        {/* <div className="clipboard-section">
           <input 
             type="text" 
             readOnly 
-            value={displayedPath || 'Select JSON key/value...'} 
+            value={selectedPath ? `event.${selectedPath}` : 'Select JSON key/value...'} 
           />
           <label className="checkbox-label">
             <input 
@@ -99,9 +154,21 @@ function App() {
             />
             Prepend event
           </label>
-          <button onClick={handleCopyClick}>📋 Copy</button>
-        </div>
-        {selectedPayload && displayJSON(selectedPayload)}
+          <button onClick={() => navigator.clipboard.writeText(`event.${selectedPath}`)}>📋 Copy</button>
+        </div> */}
+        {selectedPayload && <PayloadDisplay obj={selectedPayload} onKeyClick={handleKeyClick} />}
+      </div>
+      <div className="right-pane">
+        <RuleCreator 
+          path={path}
+          ruleName={ruleName}
+          condition={condition}
+          ruleValue={ruleValue}
+          onGenerate={generateRule}
+          onChange={handleChange}
+        />
+        <textarea readOnly value={generatedRule} />
+        <button onClick={handleCopyRule}>Copy Rule</button>
       </div>
     </div>
   );
